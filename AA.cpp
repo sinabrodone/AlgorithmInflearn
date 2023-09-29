59eaf2e0801e63423117bcf42e36ccc674406866
Jimin Kim <bmdok0719@gmail.com>
2023-09-29 10:23:42 +0900

    알고리즘 첫 커밋

---
+++ AA.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main()
+{
+	//rt -> read text 
+	//freopen("input.txt", "rt", stdin);
+
+	// 두번째 커밋 되는지 테스트 
+	return 0;
+}

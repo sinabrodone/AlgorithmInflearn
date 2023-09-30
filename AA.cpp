406e05e5de87164875a7929644d3f736bfe132fd
Jimin Kim <bmdok0719@gmail.com>
2023-09-30 10:14:34 +0900

    9/30 알고리즘 공부

--- AA.cpp
+++ AA.cpp
@@ -1,12 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main()
+ 
+int main() 
 {
-	//rt -> read text 
-	//freopen("input.txt", "rt", stdin);
-
-	// 두번째 커밋 되는지 테스트 
+ 	int n;
+ 	cin>> n;
+ 	
+ 	int count = 0;
+ 	while(n>0)
+ 	{
+ 		count++;
+ 		n-=4;	
+	}
+	
+	for(int i = 0; i<count; i++)
+	{
+		cout<< "long ";
+	}
+	
+	cout<<"int";
 	return 0;
 }

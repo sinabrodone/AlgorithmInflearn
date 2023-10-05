5f3e8cfec1ede47291c020381c9ab2b662b14dad
Jimin Kim <bmdok0719@gmail.com>
2023-10-05 13:33:29 +0900

    231005 문자열

--- AA.cpp
+++ AA.cpp
@@ -1,37 +1,10 @@
 #include <iostream>
-#include <climits>
+#include <string>
 using namespace std;
-
 int main() 
 {
-	int n, m;
-	cin>>n>>m;
+	char a;
+	cin>>a;
 	
-	int* baskets = new int[n];
-
-	//√ ±‚»≠ [1] [2] [3] [4] [5]
-	for(int i = 0; i<n;i++)
-	{
-		baskets[i] = i+1;
-	}
-	
-	int a,b;
-	for(int i = 0 ;i < m; i++)
-	{
-		cin>>a>>b;
-		a -= 1; b -=1;
-		for(int j = a; j < b / 2 ; j++)
-		{
-			int temp = baskets[j];
-			baskets[j] = baskets[b - j - 1];
-			baskets[b-j - 1] = temp;
-		}
-	}
-	
-	for(int i = 0; i<n; i++)
-	{
-		cout<<baskets[i]<<' ';
-	}
-	
-	delete[] baskets;
+	printf("%d", a);
 }
